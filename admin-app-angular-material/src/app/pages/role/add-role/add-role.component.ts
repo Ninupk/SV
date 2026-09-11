a9efe62b632b558e26d3490125562c8810d56413
Ninupk <pkninu@gmail.com>
2026-09-11 19:25:04 +0530

    nn

--- admin-app-angular-material/src/app/pages/role/add-role/add-role.component.ts
+++ admin-app-angular-material/src/app/pages/role/add-role/add-role.component.ts
@@ -1,15 +1,71 @@
 import { Component } from '@angular/core';
-import { MatCardHeader, MatCard, MatCardTitle } from "@angular/material/card";
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+
+import { CommonModule } from '@angular/common';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 
 @Component({
   selector: 'app-add-role',
   standalone: true,
-  imports: [MatCardHeader, MatCard, MatCardTitle, MatCardModule, MatFormFieldModule],
+
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule
+  ],
+
   templateUrl: './add-role.component.html',
   styleUrl: './add-role.component.scss'
 })
 export class AddRoleComponent {
+
+  roleForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+    this.roleForm = this.fb.group({
+
+      name: [
+        '',
+        Validators.required
+      ],
+
+      description: [
+        ''
+      ]
+
+    });
+
+  }
+
+
+  saveRole(): void {
+
+    if (this.roleForm.invalid) {
+
+      this.roleForm.markAllAsTouched();
+
+      return;
+    }
+
+    console.log('Role Data:', this.roleForm.value);
+
+  }
 
 }
