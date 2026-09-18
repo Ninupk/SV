83d8cc61fc95196622c4ad04ea4a1c84fd4bd51c
Ninupk <pkninu@gmail.com>
2026-09-18 17:03:01 +0530

    user view

--- admin-app-angular-material/src/app/pages/user-list/user-view/user-view.component.ts
+++ admin-app-angular-material/src/app/pages/user-list/user-view/user-view.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { MatDialogContent } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-view',
   standalone: true,
-  imports: [MatDialogContent],
+  imports: [
+    MatDialogContent, 
+    MatDialogModule,
+    CommonModule,
+  ],
   templateUrl: './user-view.component.html',
   styleUrl: './user-view.component.scss'
 })
